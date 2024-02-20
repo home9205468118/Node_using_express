60596c223df51c682afbd9ed35564aa7b0d3a960
home9205468118 <home9205468118@gmail.com>
2024-02-20 23:04:22 +0530

    post request

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ let app = express();
 let movies = JSON.parse(fs.readFileSync("./data/movies.json"))
 
 
+ app.use(express.json())
 
-
+// GET request
 app.get('/api/v1/movies', (req,res) => {
     console.log("data updated");
     res.status(200).json({
@@ -22,8 +23,17 @@ app.get('/api/v1/movies', (req,res) => {
 })
 
 app.post('/api/v1/movies', (req,res) => {
-    console.log(req.body);
-    res.send("created")
+    const newId = movies[movies.length - 1].id+1;
+    const newMovie = Object.assign({id:newId},req.body)
+    movies.push(newMovie)
+    fs.writeFile("./data/movies.json",JSON.stringify(movies),(err) => {
+        res.status(201).json({
+            status: 'success',
+            data:{
+                movie : newMovie
+            }
+        })
+    })
 })
 
 
